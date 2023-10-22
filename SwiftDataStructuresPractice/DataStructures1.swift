a59c476f684763c4c3f126abd661e445397e0ebb
Jordan Haynes <88909614+JordanCautious@users.noreply.github.com>
2023-10-22 06:19:18 -0400

    Made each of the current data structures be randomized, with the exception of the struct

--- SwiftDataStructuresPractice/DataStructures1.swift
+++ SwiftDataStructuresPractice/DataStructures1.swift
@@ -10,21 +10,35 @@ import SwiftUI
 
 struct DataStructures1 {
     func swiftArrays() {
-        var integerArray = [1, 2, 3, 4, 5]
-        integerArray.append(6)  // Adds 6 to the end
-        integerArray.remove(at: 0)  // Removes 1 from the array
+        var integerArray = (1...10).map { _ in Int.random(in: 1...100) }
+        integerArray.append(6)
+        integerArray.remove(at: 0)
         print(integerArray)
     }
 
     func swiftDictionaries() {
-        var fruitColors: [String: String] = ["Apple": "Red", "Banana": "Yellow"]
+        let possibleKeys = ["Apple", "Banana", "Cherry", "Date", "Elderberry"]
+        let possibleValues = ["Red", "Yellow", "Purple", "Brown", "Black"]
+
+        var fruitColors: [String: String] = [:]
+
+        for _ in 1...5 {
+            if let randomKey = possibleKeys.randomElement(), let randomValue = possibleValues.randomElement() {
+                fruitColors[randomKey] = randomValue
+            }
+        }
+        
         fruitColors["Orange"] = "Orange"
         fruitColors.removeValue(forKey: "Apple")
         print(fruitColors)
     }
     
     func swiftSets() {
-        var primeNumbers: Set = [2, 3, 5, 7]
+        var primeNumbers: Set<Int> = []
+        for _ in 1...10 {
+            primeNumbers.insert(Int.random(in: 1...100))
+        }
+        
         primeNumbers.insert(11)
         primeNumbers.remove(2)
         print(primeNumbers)
