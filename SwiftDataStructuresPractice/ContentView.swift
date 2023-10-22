7b615be54b6fd7c007883023623fcb426c069388
Jordan Haynes <88909614+JordanCautious@users.noreply.github.com>
2023-10-22 05:28:34 -0400

    Initial Commit

---
+++ SwiftDataStructuresPractice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftDataStructuresPractice
+//
+//  Created by Jordan Haynes on 10/22/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftDataStructuresPractice/SwiftDataStructuresPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataStructuresPracticeApp.swift
+//  SwiftDataStructuresPractice
+//
+//  Created by Jordan Haynes on 10/22/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataStructuresPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
